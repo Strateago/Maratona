5308e375f8a8940f409085510a7024e53c960a83
Thiago Costa <104111765+Strateago@users.noreply.github.com>
2023-12-08 01:04:21 -0300

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -9,29 +9,23 @@ using namespace std;
 const ll INF = LLONG_MAX;
 int v, e;
 
-pair <vector <ll>, vector <ll>> dijkstra (ll origin, vector <vector <pll>> grafo){
-    int no, peso;
+pair <vector <ll>, vector <ll>> dijkstra (ll origin, vector <pll> *grafo){
     vector <ll> dist, prec;
-    vector <pll> e;
     pll aux;
-    dist.assign(grafo.size(), INF);
-    prec.assign(grafo.size(), -1);
+    dist.assign(n, INF);
+    prec.assign(n, -1);
     dist[origin] = 0;
     priority_queue <pll> pq;
     pq.push({origin, 0});
     while(!pq.empty()){
         aux = pq.top();
         pq.pop();
-        e = grafo[aux.first];
-        for(int a = 0; a < e.size(); a++){
-            no = e[a].first;
-            peso = e[a].second;
-            if(peso + aux.second < dist[no]){
-                dist[no] = peso + aux.second;
-                prec[no] = aux.first;
-                pq.push({no, dist[no]});
+        for(pll H : grafo[aux.first]){
+            if(H.second + aux.second < dist[H.first]){
+                dist[H.first] = H.second + aux.second;
+                pq.push({H.first, dist[H.first]});
             }
         }
-        return {dist, prec};
     }
+    return {dist, prec};
 }
